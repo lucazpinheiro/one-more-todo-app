5406fec59e6ead27740c4ced53c2d3d7e8f18f66
Lucas <lucascpinheiro10@gmail.com>
2022-01-26 19:25:54 -0300

    setting up project, rendering hardcoded task list in single component, lot of things to do

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        taskList: [
+          { id: 1, title: 'aaa', description: "descrição da task aaa", completed: false },
+          { id: 1, title: 'bbb', description: "descrição da task bbb", completed: false },
+          { id: 1, title: 'ccc', description: "descrição da task ccc", completed: false },
+        ]
+      }
+    },
+    methods: {
+      handleDoneBtnClick(event) {
+        console.log(this.taskList)
+      }
+    }
+  }
+</script>
+<template>
+  <h1>todo</h1>
+  <div>
+    <div v-for="task in taskList">
+      <div>
+        <b>{{ task.title }}</b>
+        <p>{{ task.description }}</p>
+        <button @click="handleDoneBtnClick">Done!</button>
+      </div>
+      <br>
+    </div>
+  </div>
+</template>
+
